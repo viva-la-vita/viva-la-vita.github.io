39af7594606cb4eebc57ea53e636d24d5984cfb9
Songchen Tan <songchen@mit.edu>
2022-11-29 17:30:19 +0000

    Fix workflow

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,23 +1,15 @@
 const axios = require('axios');
 const fs = require('fs');
 
-require('dotenv').config({
-  path: `.env`,
-});
-
 const preprocess = (article, dev) => {
   const yaml = `---\ntitle: ${article.title}\nsidebar_position: ${article.order + dev}\n---\n`;
   const body = article.related.content.replaceAll(/^(#+)/gm, '#$1');
   return `${yaml}\n${body}`;
 }
 
-
 (async () => {
   const { data } = await axios.get(`/navigation/render/2`, {
     baseURL: 'https://api.viva-la-vita.org/api',
-    headers: {
-      'Authorization': `Bearer ${process.env.STRAPI_TOKEN}`,
-    },
     params: {
       type: 'TREE'
     }
