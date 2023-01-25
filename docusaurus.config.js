ff10047456b6c2b8c28a03ca986ec6e4daa6c7bd
Songchen Tan <i@tansongchen.com>
2023-01-24 20:54:31 -0500

    Migrate to Cloudflare Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,11 +1,4 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
-const fs = require('fs');
-const meta = JSON.parse(fs.readFileSync('meta.json', { encoding: 'utf-8' }));
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -18,175 +11,95 @@ const config = {
   favicon: 'favicon.ico',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'viva-la-vita', // Usually your GitHub org/user name.
-  projectName: 'viva-la-vita.github.io', // Usually your repo name.
+  organizationName: 'viva-la-vita',
+  projectName: 'viva-la-vita.github.io',
+  trailingSlash: false,
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  // Internalization
   i18n: {
     defaultLocale: 'zh-Hans',
     locales: ['zh-Hans'],
   },
 
   plugins: [
+    '@docusaurus/plugin-content-pages',
     [
-      '@docusaurus/plugin-pwa',
-      {
+      'docusaurus-plugin-remote-docs',
+      /** @type {import('docusaurus-plugin-remote-docs').Options} */
+      ({
+        routeBasePath: '/',
+        sidebarPath: require.resolve('./sidebars.js'),
+        downloader: require('viva-la-vita').downloadMeta,
+      }),
+    ],
+    [
+      'docusaurus-plugin-pwa-generator',
+      /** @type {import('docusaurus-plugin-pwa-generator').Options} */
+      ({
         debug: true,
         offlineModeActivationStrategies: [
           'appInstalled',
           'standalone',
           'queryString',
         ],
-        pwaHead: [
-          {
-            tagName: 'link',
-            rel: 'icon',
-            href: '/apple-icon-180.png',
-          },
-          {
-            tagName: 'link',
-            rel: 'manifest',
-            href: '/manifest.json',
-          },
-          {
-            tagName: 'meta',
-            name: 'theme-color',
-            content: 'rgb(255, 136, 136)',
-          },
-          {
-            tagName: 'meta',
-            name: 'apple-mobile-web-app-capable',
-            content: 'yes',
-          },
-          {
-            tagName: 'meta',
-            name: 'apple-mobile-web-app-status-bar-style',
-            content: '#000',
-          },
-          {
-            tagName: 'link',
-            rel: 'apple-touch-icon',
-            href: '/apple-icon-180.png',
-          },
-        ].concat(meta),
-      },
-    ],
-  ],
-
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
+        partialManifest: {
+          "name": "生如夏花知识库",
+          "short_name": "生如夏花知识库",
+          "theme_color": "#ff7777",
+          "background_color": "#424242",
+          "display": "standalone",
+          "scope": "./",
+          "start_url": "./index.html"
         },
-        blog: false,
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
+        generatorInput: {
+          source: './static/favicon.ico',
+          options: { log: false }
+        }
       }),
     ],
+    '@docusaurus/plugin-sitemap',
+    [
+      '@docusaurus/plugin-google-gtag',
+      /** @type {import('@docusaurus/plugin-google-gtag').Options} */
+      ({
+        trackingID: 'G-9VRE8Q9M2G'
+      })
+    ]
+  ],
+
+  themes: [
+    [
+      '@docusaurus/theme-classic',
+      /** @type {import('@docusaurus/theme-classic').Options} */
+      ({
+        customCss: require.resolve('./src/css/custom.css'),
+      }),
+    ]
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  /** @type {import('@docusaurus/types').ThemeConfig & import('@docusaurus/theme-search-algolia').UserThemeConfig} */
     ({
       navbar: {
         title: '生如夏花',
         logo: {
           alt: '生如夏花',
-          src: 'favicon.ico',
+          src: 'favicon-100x100.ico',
           width: 32,
           height: 32,
         },
         items: [
-          {
-            type: 'doc',
-            docId: 'introduction/index',
-            position: 'left',
-            label: '关于',
-          },
-          {
-            type: 'doc',
-            docId: 'development/index',
-            position: 'left',
-            label: '开发',
-          },
-          {
-            href: 'https://wiki.viva-la-vita.org',
-            label: '知识库',
-            position: 'right',
-          },
-          {
-            href: 'https://bbs.viva-la-vita.org',
-            label: '论坛',
-            position: 'right',
-          },
-          {
-            href: 'https://github.com/viva-la-vita',
-            label: 'GitHub',
-            position: 'right',
-          },
-          {
-            href: 'https://afdian.net/@vivalavita',
-            label: '爱发电',
-            position: 'right',
-          },
+          ...[['关于', 'introduction'], ['开发', 'development']].map(([ label, sidebarId ]) => ({ type: 'docSidebar', position: 'left', label, sidebarId })),
+          ...[['知识库', 'https://wiki.viva-la-vita.org'], ['论坛', 'https://bbs.viva-la-vita.org'], ['GitHub', 'https://github.com/viva-la-vita'], ['爱发电', 'https://afdian.net/@vivalavita']].map(([ label, href ]) => ({ position: 'right', label, href }))
         ],
       },
       footer: {
         style: 'dark',
-        // links: [
-        //   {
-        //     title: 'Docs',
-        //     items: [
-        //       {
-        //         label: 'Tutorial',
-        //         to: '/docs/intro',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'Community',
-        //     items: [
-        //       {
-        //         label: 'Stack Overflow',
-        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //       },
-        //       {
-        //         label: 'Discord',
-        //         href: 'https://discordapp.com/invite/docusaurus',
-        //       },
-        //       {
-        //         label: 'Twitter',
-        //         href: 'https://twitter.com/docusaurus',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'More',
-        //     items: [
-        //       {
-        //         label: 'Blog',
-        //         to: '/blog',
-        //       },
-        //       {
-        //         label: 'GitHub',
-        //         href: 'https://github.com/facebook/docusaurus',
-        //       },
-        //     ],
-        //   },
-        // ],
-        copyright: `CC-BY-SA 4.0 © 2019 - ${new Date().getFullYear()} 生如夏花开发者`,
+        copyright: `CC-BY-SA 4.0 © 2019 - ${new Date().getFullYear()} 生如夏花开发者及创作者`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: require('prism-react-renderer/themes/github'),
+        darkTheme: require('prism-react-renderer/themes/dracula'),
       },
     }),
 };
