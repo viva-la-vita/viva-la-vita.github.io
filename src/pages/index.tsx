0afeed2a29e7f3f61871733c3597dbae228fe924
cyh1001 <yihancao1001@gmail.com>
2026-06-18 19:18:35 -0400

    隐藏不必要的填充内容

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,8 +92,8 @@ export default function Home(): JSX.Element {
       description="探索美好而新奇的性体验">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
-        <hr />
+        {/* <HomepageFeatures /> */}
+        {/* <hr /> */}
         <HomepageManifesto />
         <hr />
         <HomepageAfdian />
